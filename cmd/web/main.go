08e199433f9c1e5809250c812107103866d4141f
Hashira21 <balkarov03@gmail.com>
2026-01-09 00:17:23 +0300

    chapter 2.8. Refactoring code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-// Объявляем домашний обработчик, который печатает слайс байтов, содержащий
-// "Hello from Snippetbox" в качестве тела ответа
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
-}
-
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new snippet..."))
-}
-
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Save a new snippet..."))
-}
 
 func main() {
 	//Используем функцию http.NewServeMux() для инициализации нового роутера
